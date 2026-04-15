12b22554187464c041cc1236f6b5d4ccacce3006
Kek-san <kylelle96@gmail.com>
2026-04-15 15:12:36 +0800

    Create new Unity Project & Create a GridSystem2D

---
+++ Assets/Scripts/Match3.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class Match3 : MonoBehaviour {
+    [SerializeField] int width = 8;
+    [SerializeField] int height = 8;
+    [SerializeField] float cellsize = 1f;
+    [SerializeField] Vector3 originPosition = Vector3.zero;
+    [SerializeField] bool debug = true;
+
+    GridSystem2D<GridObject<Gem>> grid;
+
+    void Start() {
+        //create a grid system
+        grid = GridSystem2D<GridObject<Gem>>.HorizontalGrid(width, height, cellsize, originPosition, debug);
+
+
+    }
+}
